d9750734f82b6158a9dfa4513704e10bec7422f4
WPardue <parduew@etsu.edu>
2025-11-16 19:00:29 -0500

    Added SQLAlchemy Model for the Feats Table

---
+++ feat_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Feats(Base):
+    __tablename__ = "Feats"
+
+    Feat_ID = Column(Integer, primary_key=True)
+    Name = Column(String)
+    Abillity_Req = Column(String)
+    Level_Req = Column(Integer)
+    Class_Req = Column(String)
+    Race_Req = Column(String)
+    Misc_Req = Column(String)
+    Source = Column(String)
